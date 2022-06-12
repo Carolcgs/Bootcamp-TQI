bc6c39d6fe1a39bc3bdeeac34a82ace5970c6021
Caroline Gonçalves <carol.cgsma@gmail.com>
2022-06-12 11:56:58 -0300

    adicionando arquivos

---
+++ JavaScript/introdução javascript/contador/assets/js/script.js
@@ -0,0 +1,13 @@
+var num=0
+function mais (){
+   num++
+   var numAtual =window.document.querySelector("#resultado")
+   numAtual.innerHTML =num   
+}
+function menos (){
+   num--
+   var numAtual =window.document.querySelector("#resultado")
+   numAtual.innerHTML =num
+   
+    
+}
